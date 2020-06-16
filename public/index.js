cfe83cebfd043e14fd08a6dc7f50b7f00a90efc5
laurajni <laurajni@users.noreply.github.com>
2020-06-15 23:02:33 -0700

    index is now login page!

--- public/index.js
+++ public/index.js
@@ -1,22 +1,22 @@
 const btn = document.getElementById('sub');
 
 btn.addEventListener("click", ()=>{
-	const input = document.getElementById('id?');
-	const sql = input.value;
-	const data = {sql};
-	const options = {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(data)
-	};
+    const input = document.getElementById('id?');
+    const sql = input.value;
+    const data = {sql};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
 
-	fetch('/sqlhandle', options).then((response)=>{
-		return response.json();
-	}).then((json)=>{
-		console.log(json);
-	}).catch((err)=>{
-		console.log(err);
-	});
+    fetch('/sqlhandle', options).then((response)=>{
+        return response.json();
+    }).then((json)=>{
+        console.log(json);
+    }).catch((err)=>{
+        console.log(err);
+    });
 });
