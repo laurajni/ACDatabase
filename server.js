6d8feb75d7846869b69d53411fda6b86619d5198
laurajni <laurani@live.com>
2020-06-17 22:15:25 -0700

    bug fixes & formatting

--- server.js
+++ server.js
@@ -4,30 +4,32 @@ var express = require('express');
 // server functions
 const app = express();
 app.listen('3000', () => {
-  console.log('server started!');
+    console.log('server started!');
 });
 app.use(express.static('public'));
 app.use(express.json());
 
 // query handling
-app.post('/sqlhandle', async (request, response)=>{
-  const sql = request.body.sql;
-  con.query(sql, (err, res) => {
-    if (err) console.log(err);
-    console.log(res);
-    response.json(res);
-  });
+app.post('/sqlhandle', async (request, response) => {
+    const sql = request.body.sql;
+    con.query(sql, (err, res) => {
+        if (err) console.log(err);
+        console.log(res);
+        response.json(res);
+    });
 });
 
 // database functions
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "anna",
-  database: "acnh"
+    host: "localhost",
+    user: "root",
+    password: "anna",
+    database: "acnh"
 });
 
 con.connect((err) => {
-  if (err) {console.log('mySQL not connected');}
-  console.log("Connected!");
+    if (err) {
+        console.log('mySQL not connected');
+    }
+    console.log("Connected!");
 });
